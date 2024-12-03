0755101c7bd32ab5f4c1655f0bcc01fdea6dde8c
VictorMorais2007 <00001087242678sp@al.educacao.sp.gov.br>
2024-12-03 16:10:56 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'Qual a formula de pitágoras?',
+    'hipotenusa2 = cateto2 + cateto2'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Alemanha?',
+    'A capital da Alemanha é Berlim'
+)
+
+criaCartao(
+    'Física',
+    'Quantos planetas tem no sistema solar?',
+    'tem 8 planetas'
+)
+
+criaCartao(
+    'Fisica',
+    'O que é calor específico?',
+    'quantidade de calor necessária para que cada grama de uma substância sofra uma variação de temperatura correspondente a 1°C.'
+)
